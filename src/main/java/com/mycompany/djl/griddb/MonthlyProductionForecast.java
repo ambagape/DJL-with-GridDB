1a01747758592501c348085cf79aa48c18e874bb
Ambrose Ariagiegbe <ambagape@gmail.com>
2023-09-19 19:57:20 +0100

    Introduce custom dataset type

--- src/main/java/com/mycompany/djl/griddb/MonthlyProductionForecast.java
+++ src/main/java/com/mycompany/djl/griddb/MonthlyProductionForecast.java
@@ -1,13 +1,21 @@
 package com.mycompany.djl.griddb;
 
 import ai.djl.Model;
+import ai.djl.ModelException;
+import ai.djl.basicdataset.tabular.utils.Feature;
+import ai.djl.inference.Predictor;
 import ai.djl.metric.Metrics;
 import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDArrays;
+import ai.djl.ndarray.NDList;
 import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.types.DataType;
 import ai.djl.ndarray.types.Shape;
 import ai.djl.nn.Parameter;
+import ai.djl.timeseries.Forecast;
 import ai.djl.timeseries.TimeSeriesData;
 import ai.djl.timeseries.dataset.FieldName;
+import ai.djl.timeseries.dataset.TimeFeaturizers;
 import ai.djl.timeseries.distribution.DistributionLoss;
 import ai.djl.timeseries.distribution.output.DistributionOutput;
 import ai.djl.timeseries.distribution.output.NegativeBinomialOutput;
@@ -15,20 +23,25 @@ import ai.djl.timeseries.evaluator.Rmsse;
 import ai.djl.timeseries.model.deepar.DeepARNetwork;
 import ai.djl.timeseries.timefeature.TimeFeature;
 import ai.djl.timeseries.transform.TimeSeriesTransform;
+import ai.djl.timeseries.translator.DeepARTranslator;
 import ai.djl.training.DefaultTrainingConfig;
 import ai.djl.training.EasyTrain;
 import ai.djl.training.Trainer;
-import ai.djl.training.dataset.ArrayDataset;
+import ai.djl.training.TrainingResult;
+import ai.djl.training.dataset.Batch;
 import ai.djl.training.dataset.Dataset;
 import ai.djl.training.initializer.XavierInitializer;
+import ai.djl.training.listener.SaveModelTrainingListener;
+import ai.djl.training.listener.TrainingListener;
+import ai.djl.training.loss.Loss;
+import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.TranslateException;
+import ai.djl.util.Progress;
 import com.google.gson.GsonBuilder;
+import com.mycompany.djl.griddb.datasets.GridDBDataset;
 import com.mycompany.djl.griddb.db.DB.Entry;
 import com.toshiba.mwcloud.gs.GSException;
 import com.toshiba.mwcloud.gs.GridStore;
-import com.toshiba.mwcloud.gs.GridStoreFactory;
-import com.toshiba.mwcloud.gs.Query;
-import com.toshiba.mwcloud.gs.RowSet;
 import com.toshiba.mwcloud.gs.TimeSeries;
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,10 +49,13 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
@@ -47,18 +63,19 @@ import java.util.Properties;
  */
 public class MonthlyProductionForecast {
 
-    final static String FREQ = "M";
-    final static int PREDICTION_LENGTH = 12;
+    final static String FREQ = "W";
+    final static int PREDICTION_LENGTH = 1;
+    final static LocalDateTime START_TIME = LocalDateTime.parse("1985-01-01T00:00");
+    final static String MODEL_OUTPUT_DIR = "outputs";
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting...");
 
         GridStore store = null;
         try {
-            store = connectToGridDB();
-            seedDatabase(store);
-            startTraining(store);
-            predict();
+            store = GridDBDataset.connectToGridDB();
+            seedDatabase();
+            startTraining();
         } finally {
             if (store != null) {
                 try {
@@ -70,20 +87,65 @@ public class MonthlyProductionForecast {
         }
     }
 
-    public static float[] predict() throws Exception {
-        return null;
+    public static Map<String, Float> predict(String outputDir)
+            throws IOException, TranslateException, ModelException {
+        try ( Model model = Model.newInstance("deepar")) {
+            DeepARNetwork predictionNetwork = getDeepARModel(new NegativeBinomialOutput(), false);
+            model.setBlock(predictionNetwork);
+            model.load(Paths.get(MODEL_OUTPUT_DIR));
+
+            Dataset testSet
+                    = getDataset(Dataset.Usage.TEST, predictionNetwork.getContextLength(), new ArrayList<>());
+
+            Map<String, Object> arguments = new ConcurrentHashMap<>();
+            arguments.put("prediction_length", 1); // Univariate, so predict one value at a time
+            arguments.put("freq", "W");
+            arguments.put("use_" + FieldName.FEAT_DYNAMIC_REAL.name().toLowerCase(), false);
+            arguments.put("use_" + FieldName.FEAT_STATIC_CAT.name().toLowerCase(), false);
+            arguments.put("use_" + FieldName.FEAT_STATIC_REAL.name().toLowerCase(), false);
+            DeepARTranslator translator = DeepARTranslator.builder(arguments).build();
+
+            M5Evaluator evaluator
+                    = new M5Evaluator(0.5f, 0.67f, 0.95f, 0.99f);
+            Progress progress = new ProgressBar();
+            try ( Predictor<TimeSeriesData, Forecast> predictor = model.newPredictor(translator)) {
+                for (Batch batch : testSet.getData(model.getNDManager().newSubManager())) {
+                    NDList data = batch.getData();
+                    NDArray target = data.head();
+
+                    NDArray gt = target.get(":, {}:", -PREDICTION_LENGTH);
+                    NDArray pastTarget = target.get(":, :{}", -PREDICTION_LENGTH);
+
+                    NDList gtSplit = gt.split(batch.getSize());
+                    NDList pastTargetSplit = pastTarget.split(batch.getSize());
+
+                    List<TimeSeriesData> batchInput = new ArrayList<>(batch.getSize());
+                    for (int i = 0; i < batch.getSize(); i++) {
+                        TimeSeriesData input = new TimeSeriesData(10);
+                        input.setStartTime(START_TIME);
+                        input.setField(FieldName.TARGET, pastTargetSplit.get(i).squeeze(0));
+                        batchInput.add(input);
+                    }
+                    List<Forecast> forecasts = predictor.batchPredict(batchInput);
+
+                    for (int i = 0; i < forecasts.size(); i++) {
+                        evaluator.aggregateMetrics(
+                                evaluator.getMetricsPerTs(
+                                        gtSplit.get(i).squeeze(0),
+                                        pastTargetSplit.get(i).squeeze(0),
+                                        forecasts.get(i)));
+                    }
+
+                    progress.increment(batch.getSize());
+                    batch.close();
+                }
+            }
+
+            return evaluator.computeTotalMetrics();
+        }
     }
 
-    private static GridStore connectToGridDB() throws GSException {
-        Properties props = new Properties();
-        props.setProperty("notificationMember", "127.0.0.1:10001");
-        props.setProperty("clusterName", "defaultCluster");
-        props.setProperty("user", "admin");
-        props.setProperty("password", "admin");
-        return GridStoreFactory.getInstance().getGridStore(props);
-    }
-
-    private static void startTraining(GridStore store) throws IOException, TranslateException {
+    private static void startTraining() throws IOException, TranslateException, Exception {
 
         DistributionOutput distributionOutput = new NegativeBinomialOutput();
 
@@ -98,8 +160,7 @@ public class MonthlyProductionForecast {
 
             List<TimeSeriesTransform> trainingTransformation = trainingNetwork.createTrainingTransformation(manager);
 
-            List<TimeSeriesData> data = loadMonthlyProductionData(store, trainingTransformation, manager);
-            Dataset trainSet = getDataset(data);
+            Dataset trainSet = getDataset(Dataset.Usage.TRAIN, trainingNetwork.getContextLength(), trainingTransformation);
 
             trainer = model.newTrainer(setupTrainingConfig(distributionOutput));
             trainer.setMetrics(new Metrics());
@@ -107,7 +168,7 @@ public class MonthlyProductionForecast {
             int historyLength = trainingNetwork.getHistoryLength();
             Shape[] inputShapes = new Shape[9];
             // (N, num_cardinality)
-            inputShapes[0] = new Shape(1, 5);
+            inputShapes[0] = new Shape(1, 1);
             // (N, num_real) if use_feat_stat_real else (N, 1)
             inputShapes[1] = new Shape(1, 1);
             // (N, history_length, num_time_feat + num_age_feat)
@@ -115,11 +176,10 @@ public class MonthlyProductionForecast {
             inputShapes[3] = new Shape(1, historyLength);
             inputShapes[4] = new Shape(1, historyLength);
             inputShapes[5] = new Shape(1, historyLength);
-            inputShapes[6] = new Shape(1, PREDICTION_LENGTH, TimeFeature.timeFeaturesFromFreqStr(FREQ).size() + 1);
-            inputShapes[7] = new Shape(1, PREDICTION_LENGTH);
-            inputShapes[8] = new Shape(1, PREDICTION_LENGTH);
+            inputShapes[6] = new Shape(1, 1, TimeFeature.timeFeaturesFromFreqStr(FREQ).size() + 1);
+            inputShapes[7] = new Shape(1, 1);
+            inputShapes[8] = new Shape(1, 1);
             trainer.initialize(inputShapes);
-
             int epoch = 10;
             EasyTrain.fit(trainer, epoch, trainSet, null);
         } finally {
@@ -146,58 +206,151 @@ public class MonthlyProductionForecast {
     }
 
     private static DefaultTrainingConfig setupTrainingConfig(DistributionOutput distributionOutput) {
+
+        SaveModelTrainingListener listener = new SaveModelTrainingListener(MODEL_OUTPUT_DIR);
+        listener.setSaveModelCallback(
+                trainer -> {
+                    TrainingResult result = trainer.getTrainingResult();
+                    Model model = trainer.getModel();
+                    float rmsse = result.getValidateEvaluation("RMSSE");
+                    model.setProperty("RMSSE", String.format("%.5f", rmsse));
+                    model.setProperty("Loss", String.format("%.5f", result.getValidateLoss()));
+                });
+
         return new DefaultTrainingConfig(new DistributionLoss("Loss", distributionOutput))
                 .addEvaluator(new Rmsse(distributionOutput))
-                .optInitializer(new XavierInitializer(), Parameter.Type.WEIGHT);
+                .optInitializer(new XavierInitializer(), Parameter.Type.WEIGHT)
+                .addTrainingListeners(TrainingListener.Defaults.logging(MODEL_OUTPUT_DIR))
+                .addTrainingListeners(listener);
     }
 
-    private static ArrayDataset getDataset(
-            List<TimeSeriesData> data) throws IOException {
+    private static Dataset getDataset(Dataset.Usage usage,
+            int contextLength,
+            List<TimeSeriesTransform> transformation) throws IOException {
 
-        NDArray[] inputList = new NDArray[data.size()];
-        NDArray[] targetList = new NDArray[data.size()];
-        for (int i = 0; i < data.size(); i++) {
-            TimeSeriesData timeSeriesData = data.get(i);
-            NDArray input = timeSeriesData.get(FieldName.FEAT_DYNAMIC_REAL).toDense();
-            NDArray target = timeSeriesData.get(FieldName.TARGET).toDense();
-            inputList[i] = input;
-            targetList[i] = target;
+        GridDBDataset.GridDBBuilder builder
+                = GridDBDataset.builder()
+                        .optUsage(usage)
+                        .setTransformation(transformation)
+                        .setContextLength(contextLength)
+                        .initData()
+                        .setSampling(12, usage == Dataset.Usage.TRAIN);
+
+        int maxWeek = usage == Dataset.Usage.TRAIN ? builder.dataLength - 12 : builder.dataLength;
+        for (int i = 1; i <= maxWeek; i++) {
+            builder.addFeature("w_" + i, FieldName.TARGET);
         }
 
-        // Create the ArrayDataset
-        ArrayDataset dataset = new ArrayDataset.Builder()
-                .setData(inputList)
-                .optLabels(targetList)
-                .setSampling(1, true)
-                .build();
+        GridDBDataset dataset
+                = builder.addFieldFeature(FieldName.START,
+                        new Feature(
+                                "date",
+                                TimeFeaturizers.getConstantTimeFeaturizer(START_TIME)))
+                        .build();
+        dataset.prepare(new ProgressBar());
         return dataset;
-
     }
 
-    private static List<TimeSeriesData> loadMonthlyProductionData(GridStore store,
-            List<TimeSeriesTransform> trainingTransformation, NDManager manager) throws GSException {
-        TimeSeries<Entry> entries = store.getTimeSeries("ENTRIES", Entry.class);
-        Query<Entry> query = entries.query("Select * from ENTRIES");
-        RowSet<Entry> rowSet = query.getRowSet();
+    public static final class M5Evaluator {
 
-        List<TimeSeriesData> timeSeriesDataList = new ArrayList<>();
-        while (rowSet.hasNext()) {
-            TimeSeriesData timeSeriesData = new TimeSeriesData(1);
-            LocalDateTime timestamp = LocalDateTime.parse((CharSequence) rowSet.next().createdAt);
-            float value = ((Number) rowSet.next().value).floatValue();
-            timeSeriesData.setStartTime(timestamp);
-            timeSeriesData.add(FieldName.TARGET, NDManager.newBaseManager().create(value));
-            timeSeriesDataList.add(applyTransformations(trainingTransformation, timeSeriesData, manager));
+        private float[] quantiles;
+        Map<String, Float> totalMetrics;
+        Map<String, Integer> totalNum;
+
+        public M5Evaluator(float... quantiles) {
+            this.quantiles = quantiles;
+            totalMetrics = new ConcurrentHashMap<>();
+            totalNum = new ConcurrentHashMap<>();
+            init();
         }
 
-        return timeSeriesDataList;
-    }
+        public Map<String, Float> getMetricsPerTs(
+                NDArray gtTarget, NDArray pastTarget, Forecast forecast) {
+            Map<String, Float> retMetrics
+                    = new ConcurrentHashMap<>((8 + quantiles.length * 2) * 3 / 2);
+            NDArray meanFcst = forecast.mean();
+            NDArray medianFcst = forecast.median();
 
-    private static TimeSeriesData applyTransformations(List<TimeSeriesTransform> transformations, TimeSeriesData dataPoint, NDManager manager) {
-        for (TimeSeriesTransform transform : transformations) {
-            dataPoint = transform.transform(manager, dataPoint, true);
+            NDArray meanSquare = gtTarget.sub(meanFcst).square().mean();
+            NDArray scaleDenom = gtTarget.get("1:").sub(gtTarget.get(":-1")).square().mean();
+
+            NDArray rmsse = meanSquare.div(scaleDenom).sqrt();
+            rmsse = NDArrays.where(scaleDenom.eq(0), rmsse.onesLike(), rmsse);
+
+            retMetrics.put("RMSSE", rmsse.getFloat());
+
+            retMetrics.put("MSE", gtTarget.sub(meanFcst).square().mean().getFloat());
+            retMetrics.put("abs_error", gtTarget.sub(medianFcst).abs().sum().getFloat());
+            retMetrics.put("abs_target_sum", gtTarget.abs().sum().getFloat());
+            retMetrics.put("abs_target_mean", gtTarget.abs().mean().getFloat());
+            retMetrics.put(
+                    "MAPE", gtTarget.sub(medianFcst).abs().div(gtTarget.abs()).mean().getFloat());
+            retMetrics.put(
+                    "sMAPE",
+                    gtTarget.sub(medianFcst)
+                            .abs()
+                            .div(gtTarget.abs().add(medianFcst.abs()))
+                            .mean()
+                            .mul(2)
+                            .getFloat());
+            retMetrics.put("ND", retMetrics.get("abs_error") / retMetrics.get("abs_target_sum"));
+
+            for (float quantile : quantiles) {
+                NDArray forecastQuantile = forecast.quantile(quantile);
+                NDArray quantileLoss
+                        = Loss.quantileL1Loss(quantile)
+                                .evaluate(new NDList(gtTarget), new NDList(forecastQuantile));
+                NDArray quantileCoverage
+                        = gtTarget.lt(forecastQuantile).toType(DataType.FLOAT32, false).mean();
+                retMetrics.put(
+                        String.format("QuantileLoss[%.2f]", quantile), quantileLoss.getFloat());
+                retMetrics.put(
+                        String.format("Coverage[%.2f]", quantile), quantileCoverage.getFloat());
+            }
+            return retMetrics;
         }
-        return dataPoint;
+
+        public void aggregateMetrics(Map<String, Float> metrics) {
+            for (Map.Entry<String, Float> entry : metrics.entrySet()) {
+                totalMetrics.compute(entry.getKey(), (k, v) -> v + entry.getValue());
+                totalNum.compute(entry.getKey(), (k, v) -> v + 1);
+            }
+        }
+
+        public Map<String, Float> computeTotalMetrics() {
+            for (Map.Entry<String, Integer> entry : totalNum.entrySet()) {
+                if (!entry.getKey().contains("sum")) {
+                    totalMetrics.compute(entry.getKey(), (k, v) -> v / (float) entry.getValue());
+                }
+            }
+
+            totalMetrics.put("RMSE", (float) Math.sqrt(totalMetrics.get("MSE")));
+            totalMetrics.put(
+                    "NRMSE", totalMetrics.get("RMSE") / totalMetrics.get("abs_target_mean"));
+            return totalMetrics;
+        }
+
+        private void init() {
+            List<String> metricNames
+                    = new ArrayList<>(
+                            Arrays.asList(
+                                    "RMSSE",
+                                    "MSE",
+                                    "abs_error",
+                                    "abs_target_sum",
+                                    "abs_target_mean",
+                                    "MAPE",
+                                    "sMAPE",
+                                    "ND"));
+            for (float quantile : quantiles) {
+                metricNames.add(String.format("QuantileLoss[%.2f]", quantile));
+                metricNames.add(String.format("Coverage[%.2f]", quantile));
+            }
+            for (String metricName : metricNames) {
+                totalMetrics.put(metricName, 0f);
+                totalNum.put(metricName, 0);
+            }
+        }
     }
 
     private static Entry[] getTimeSeriesData(URL url) throws Exception {
@@ -220,12 +373,17 @@ public class MonthlyProductionForecast {
 
     }
 
-    private static void seedDatabase(GridStore store) throws Exception {
-        Entry[] entries = getTimeSeriesData(MonthlyProductionForecast.class.getClassLoader().getResource("data/csvjson.json"));
-        TimeSeries<Entry> timeSeries = store.putTimeSeries("ENTRIES", Entry.class);
-        for (Entry entry : entries) {
-            System.out.println(String.format("%s , %s", entry.createdAt, entry.value));
-            timeSeries.put(entry);
+    /*
+    We assume the database is already containing the timeseries data
+     */
+    private static void seedDatabase() throws Exception {
+        try ( GridStore store = GridDBDataset.connectToGridDB()) {
+            Entry[] entries = getTimeSeriesData(MonthlyProductionForecast.class.getClassLoader().getResource("data/csvjson.json"));
+            TimeSeries<Entry> timeSeries = store.putTimeSeries("ENTRIES", Entry.class);
+            for (Entry entry : entries) {
+                System.out.println(String.format("%s , %s", entry.createdAt, entry.value));
+                timeSeries.put(entry);
+            }
         }
     }
 }
---
+++ src/main/java/com/mycompany/djl/griddb/datasets/GridDBDataset.java
@@ -0,0 +1,160 @@
+package com.mycompany.djl.griddb.datasets;
+
+import ai.djl.basicdataset.tabular.utils.Feature;
+import ai.djl.timeseries.dataset.FieldName;
+import ai.djl.timeseries.dataset.M5Forecast;
+import ai.djl.timeseries.transform.TimeSeriesTransform;
+import ai.djl.util.Progress;
+import com.mycompany.djl.griddb.db.DB;
+import com.toshiba.mwcloud.gs.GSException;
+import com.toshiba.mwcloud.gs.GridStore;
+import com.toshiba.mwcloud.gs.GridStoreFactory;
+import com.toshiba.mwcloud.gs.Query;
+import com.toshiba.mwcloud.gs.RowSet;
+import com.toshiba.mwcloud.gs.TimeSeries;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ambag
+ */
+public class GridDBDataset extends M5Forecast {
+
+    private final int dataLength;
+    private final File csvFile;
+
+    protected GridDBDataset(GridDBBuilder builder) throws GSException, FileNotFoundException {
+        super(builder);
+        this.csvFile = builder.csvFile;
+        this.dataLength = builder.dataLength;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void prepare(Progress progress) throws IOException {
+        csvUrl = this.csvFile.toURI().toURL();
+        super.prepare(progress);
+    }
+
+    public int getDataLength() {
+        return this.dataLength;
+    }
+
+    public static GridStore connectToGridDB() throws GSException {
+        Properties props = new Properties();
+        props.setProperty("notificationMember", "127.0.0.1:10001");
+        props.setProperty("clusterName", "defaultCluster");
+        props.setProperty("user", "admin");
+        props.setProperty("password", "admin");
+        return GridStoreFactory.getInstance().getGridStore(props);
+    }
+
+    public static GridDBBuilder builder() {
+        GridDBBuilder builder = null;
+        try {
+            builder = new GridDBBuilder();
+        } catch (GSException ex) {
+            Logger.getLogger(GridDBDataset.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return builder;
+    }
+
+    public static class GridDBBuilder extends M5Forecast.Builder {
+
+        GridStore store;
+        public int dataLength = 0;
+        File csvFile;
+
+        GridDBBuilder() throws GSException {
+            store = connectToGridDB();
+        }
+
+        @Override
+        protected GridDBBuilder self() {
+            return this;
+        }
+
+        public GridDBBuilder optStore(GridStore store) throws GSException {
+            this.store.close();
+            this.store = store;
+            return this;
+        }
+
+        @Override
+        public GridDBBuilder addFieldFeature(FieldName name, Feature feature) {
+            super.addFieldFeature(name, feature);
+            return this;
+        }
+
+        @Override
+        public GridDBBuilder setTransformation(List<TimeSeriesTransform> transforms) {
+            super.setTransformation(transforms);
+            return this;
+        }
+
+        @Override
+        public GridDBBuilder optUsage(Usage usage) {
+            super.optUsage(usage);
+            return this;
+        }
+
+        @Override
+        public GridDBBuilder setSampling(int size, boolean isRandom) {
+            super.setSampling(size, isRandom);
+            return this;
+        }
+
+        @Override
+        public GridDBBuilder setContextLength(int size) {
+            super.setContextLength(size);
+            return this;
+        }
+
+        private File fetchDBDataAndSaveCSV(GridStore store) throws GSException, FileNotFoundException {
+            File csvOutputFile = new File("out.csv");
+            try ( GridStore store2 = store) {
+                TimeSeries<DB.Entry> entries = store2.getTimeSeries("ENTRIES", DB.Entry.class);
+                Query<DB.Entry> query = entries.query("Select * from ENTRIES");
+                RowSet<DB.Entry> rowSet = query.getRowSet();
+                dataLength = rowSet.size();
+                List<String> csv = new LinkedList<>();
+                while (rowSet.hasNext()) {
+                    csv.add(String.format("%s, %f", rowSet.next().createdAt, rowSet.next().value));
+                }
+                try ( PrintWriter pw = new PrintWriter(csvOutputFile)) {
+                    csv.stream()
+                            .forEach(pw::println);
+                }
+            }
+
+            return csvOutputFile;
+        }
+
+        public GridDBBuilder initData() throws GSException, FileNotFoundException {
+            this.csvFile = fetchDBDataAndSaveCSV(this.store);
+            return this;
+        }
+
+        @Override
+        public GridDBDataset build() {
+            GridDBDataset gridDBDataset = null;
+            try {
+                gridDBDataset = new GridDBDataset(this);
+            } catch (GSException | FileNotFoundException ex) {
+                Logger.getLogger(GridDBDataset.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return gridDBDataset;
+        }
+
+    }
+}
