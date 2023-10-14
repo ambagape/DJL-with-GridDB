fe54551e652c1a56531cba8458d73df5e852fc76
Ambrose Ariagiegbe <ambagape@gmail.com>
2023-10-14 21:26:19 +0100

    update

--- src/main/java/com/mycompany/djl/griddb/datasets/MySQLDataset.java
+++ src/main/java/com/mycompany/djl/griddb/datasets/MySQLDataset.java
@@ -128,7 +128,7 @@ public class MySQLDataset extends M5Forecast {
             try ( Statement statement = con.createStatement();  ResultSet resultSet = statement.executeQuery(sql)) {
                 List<String> csv = new LinkedList<>();
                 while (resultSet.next()) {
-                    csv.add(String.format("%s, %f", resultSet.getDate("createdAt"), resultSet.getDate("value")));
+                    csv.add(String.format("%s, %f", resultSet.getTimestamp("createdAt"), resultSet.getDate("value")));
                 }
                 try ( PrintWriter pw = new PrintWriter(csvOutputFile)) {
                     csv.stream()
